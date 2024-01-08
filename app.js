7debd2efd13a2c8de2a8e6ef7979ede2e29c263a
Sinai161 <ashleygalvan161@yahoo.com>
2024-01-07 17:59:21 -0800

    added a click event to cards

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// Add event listener so when I click on the card it turns over 
+// If the cards do not match I want them to turn back around
+// If the cards do match I want them to stay flipped over 
+// I want there to be three tries until i have to restart the game and start over
+const card = document.getElementById("card-container")
+   card.addEventListener("click",(event) => {
+    if(event.target.classList.contains("cards"))
+        alert("you clicked a card")
+   })
 
-const cardContainer = document.getElementById("card-container")
 
